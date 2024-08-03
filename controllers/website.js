c40fb53b76a980829f421d4ff00ba6d9cae67ede
Mohab Yasser <mohab.yasser11@gmail.com>
2024-08-03 17:39:40 +0300

    updated all try catch functions in website.js file

--- controllers/website.js
+++ controllers/website.js
@@ -9,12 +9,12 @@ const userSensors = require('../models/userSensors');
 exports.getHomefriends = async (req, res, next) => {
   let hf = [];
   const user = await User.findById(req.body.userId);
-    if(!user){
-      const error = new Error('A user with this email could not be found');
-      error.statusCode = 401;
-      throw error;
-    }
     try{
+      if(!user){
+        const error = new Error('A user with this email could not be found');
+        error.statusCode = 401;
+        throw error;
+      }
       await user.populate('homeFriends');
       for(let i in user.homeFriends){
         const { name, phone, email, long, late } = user.homeFriends[i];
@@ -52,12 +52,12 @@ exports.getHomefriends = async (req, res, next) => {
 exports.postAddHomeFriend = async (req,res,next) => {
     const friendId = req.body.friendId;
     const user = await User.findById(req.body.userId);
-    if(!user){
-      const error = new Error('A user with this email could not be found');
-      error.statusCode = 404;
-      throw error;
-    }
     try{
+      if(!user){
+        const error = new Error('A user with this email could not be found');
+        error.statusCode = 404;
+        throw error;
+      }
       // await user.addHomeFriend(friendId);
       const homeFriendIndex = user.homeFriends.findIndex(hm => {
         return hm.toString() === friendId.toString();
@@ -86,30 +86,39 @@ exports.postAddHomeFriend = async (req,res,next) => {
 exports.postDeleteHomeFriend = async (req,res,next) => {
   const friendId = req.body.friendId;
   const user = await User.findById(req.body.userId);
+  try{
     if(!user){
       const error = new Error('A user with this email could not be found');
       error.statusCode = 401;
       throw error;
     }
-  // await user.removeHomeFriend(friendId);
-  const updatedHomeFriends = user.homeFriends.filter(hm => {
-    return hm._id.toString() !== friendId.toString();
-  });
-  user.homeFriends = updatedHomeFriends;
-  await user.save();
-  res.status(200).json({
-      message: 'Home friend removed.'
-  });
+    // await user.removeHomeFriend(friendId);
+    const updatedHomeFriends = user.homeFriends.filter(hm => {
+      return hm._id.toString() !== friendId.toString();
+    });
+    user.homeFriends = updatedHomeFriends;
+    await user.save();
+    res.status(200).json({
+        message: 'Home friend removed.'
+    });
+  }
+  catch(err){
+    res.status(404).json("A user with this email could not be found");
+      if(!err.statusCode){
+        err.statusCode = 500;
+      }
+    next(err);
+  }
 }
 
 exports.postaddSensor = async (req,res,next) => {
   const user = await User.findById(req.body.userId);
-  if(!user){
-    const error = new Error('A user with this email could not be found');
-    error.statusCode = 401;
-    throw error;
-  }
   try{
+    if(!user){
+      const error = new Error('A user with this email could not be found');
+      error.statusCode = 401;
+      throw error;
+    }
     const usersensor = new UserSensors({
       title: req.body.title,
       userId: req.body.userId,
@@ -137,12 +146,12 @@ exports.postaddSensor = async (req,res,next) => {
 
 exports.getUserSensors = async (req,res,next) => {
   const sensors = await userSensors.find({ familyId: req.body.familyId });
-  if(!sensors){
-    const error = new Error('Could not find any sensor to this user.');
-    error.statusCode = 401;
-    throw error;
-  }
   try{
+    if(!sensors){
+      const error = new Error('Could not find any sensor to this user.');
+      error.statusCode = 401;
+      throw error;
+    }
     let s = [];
     for(let i in sensors){
       const { title, sensorId, isMaster, isDetected, description, helperText } = sensors[i];
@@ -170,12 +179,12 @@ exports.postdeleteSensor = async (req,res,next) => {
   //   });
   // }
   const user = await User.findById(req.body.userId);
-  if(!user){
-    const error = new Error('A user with this email could not be found');
-    error.statusCode = 401;
-    throw error;
-  }
   try{
+    if(!user){
+      const error = new Error('A user with this email could not be found');
+      error.statusCode = 401;
+      throw error;
+    }
     const sensor = await UserSensors.findOne({sensorId: sensorId});
     await sensor.deleteOne();
     await user.removeSensor(sensorId);
@@ -521,26 +530,25 @@ exports.receiveHealthSensor = async (req,res,next) => {
   const usersensor = await UserSensors.findOne({sensorId: id});
   const userId = usersensor.userId.toString();
   const user = await User.findById(userId);
-  if(!user){
-    const error = new Error('Could not find a user to this sensor.');
-    error.statusCode = 401;
-    throw error;
-  }
-  await user.populate('homeFriends');
-  for(let i in user.homeFriends){
-    const { FCMToken } = user.homeFriends[i];
-    receipentTokens[i] = FCMToken;
-  }
-  if(type == "heart_rate"){
-    body = "Your Heart rate is abnormal, seek an immediate assistance.";
-    homefriendBody = "Your home friend " + user.name + " heart rate is abnormal, seek an immediate assistance.";
-  }
-  else if(type == "spo2"){
-    body = "The oxygen in your blood is in abnormal state, seek an immediate assistance.";
-    homefriendBody = "Your home friend " + user.name + " blood oxygen is abnormal, seek an immediate assistance.";
-  }
-  
   try {
+    if(!user){
+      const error = new Error('Could not find a user to this sensor.');
+      error.statusCode = 401;
+      throw error;
+    }
+    await user.populate('homeFriends');
+    for(let i in user.homeFriends){
+      const { FCMToken } = user.homeFriends[i];
+      receipentTokens[i] = FCMToken;
+    }
+    if(type == "heart_rate"){
+      body = "Your Heart rate is abnormal, seek an immediate assistance.";
+      homefriendBody = "Your home friend " + user.name + " heart rate is abnormal, seek an immediate assistance.";
+    }
+    else if(type == "spo2"){
+      body = "The oxygen in your blood is in abnormal state, seek an immediate assistance.";
+      homefriendBody = "Your home friend " + user.name + " blood oxygen is abnormal, seek an immediate assistance.";
+    }
     function sendNotification(message){
       let clientServerOptions = {
         uri: 'https://fcm.googleapis.com/fcm/send',
